2c24e3564c52c1394ae483853e9abb5707909813
mateusvmv <mateusv251@gmail.com>
2024-05-15 15:38:58 -0300

    imprime tempo de processamento

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,18 @@ import Text.Printf (printf)
 import Generator
 import Utils
 import Primes
+import Data.Time
 
 main :: IO ()
 main = do
     n <- getLine
     a <- getLine
 
+    t0 <- getCurrentTime
     let p = firstPrimeGT (read n)
     let g = generator p
     print p
     print g
     print "logaritmo discreto"
-    print "Tempo de processamento??kkkkkkkkk"
+    tf <- getCurrentTime
+    print (diffUTCTime tf t0)
