d717c45280ec02b14096bc254cd807e2c4457946
mateusvmv <mateusv251@gmail.com>
2024-05-16 04:22:22 -0300

    array!

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Como executar
 `runghc -isrc src/Main.hs`
-# Compilação com Optimizações
+## Com Optimizações
 `ghc -O3 -o Main -isrc src/Main.hs`
-# Estatísticas de Execução
+## Estatísticas de Execução
 `env time -v ./Main < input`
+# Interativo
+`ghci -isrc src/Main.hs`
+## Optimizações
+`ghci -fobject-code -O3 -isrc src/Main.hs`
+## Estatísticas de Execução
+`:set +s`
--- src/Primes.hs
+++ src/Primes.hs
@@ -3,18 +3,23 @@ import Utils
 import Data.Bits
 import Data.List
 import Debug.Trace
+import Data.Array.Unboxed
+import Data.Array.ST
 
 -- Primos
 nthPrime :: Int -> Integer = (primes !!)
 primes :: [Integer] = [2,3] ++ sieve 5 where
     sieve n =
         let
-            top = min (n + 4096) (2 + n*n - 4*n)
+            top = min (n + 2^15) (2 + n*n - 4*n)
             ps' = takeWhile (\p -> p*p < top) (tail primes)
             muls p = [l, l+2*p .. top] where l = p * ((n+p-1) `div` p .|. 1)
-            multiples = foldl' (\b x -> setBit b (fromIntegral (x-n))) 0 (concat $ map muls ps') :: Integer
-            integerPrimes = [i | i <- [n, n+2 .. top], not (testBit multiples (fromIntegral (i-n)))]
-        in integerPrimes ++ (sieve (top+2))
+            multiples = runSTUArray $ do
+                arr <- newArray_ (n, top)
+                mapM_ (\i -> writeArray arr i True) (concatMap muls ps')
+                return arr
+            integerPrimes = [i | i <- [n, n+2 .. top], not $ multiples ! i]
+        in integerPrimes ++ sieve (top+2)
 
 -- Roda de fatoração
 -- Lista de coprimos dos n primeiros primos, antes do ciclo
@@ -32,7 +37,7 @@ isPrime :: Integer -> Bool
 isPrime n
   | n < 10^12 = length (factorize n) == 1
   | any (\p -> n `mod` p == 0) (take 1000 primes) = False
-  | otherwise = all (not . witness) (take 50 primes) where
+  | otherwise = (not . any witness) (take 50 primes) where
     (s, d) = f' (0, n-1) where
         f' (s, n)
           | even n = f' (s+1, shiftR n 1)
@@ -46,11 +51,11 @@ isPrime n
 -- Encontra o próximo primo, dado um número N
 firstPrimeGT :: Integer -> Integer
 firstPrimeGT n
-    | n <= wheel5Cycle = head $ dropWhile (\x -> x <= n) primes
+    | n <= wheel5Cycle = head $ dropWhile (<= n) primes
     | otherwise = let
         x = n - (n `mod` wheel5Cycle)
-        wheel = concatMap (\y -> map (\x -> x+y) wheel5) [x-wheel5Cycle, x..]
-        candidates = dropWhile (\x -> x <= n) wheel
+        wheel = concatMap (\y -> map (+y) wheel5) [x-wheel5Cycle, x..]
+        candidates = dropWhile (<= n) wheel
     in head [x | x <- candidates, isPrime x]
 
 -- Retorna a lista de fatores de um numero
@@ -75,7 +80,7 @@ factorizeHuge n = factorize' n primes
   where
     factorize' :: Integer -> [Integer] -> Factorization
     factorize' n (p:ps)
-      | p > 10^5 = Partial []
+      | p > 10^6 = Partial []
       | p * p > n = Full [n]
       | n `rem` p == 0 = case factorize' (n `div` p) (p:ps) of
         Full fs -> Full (p:fs)
