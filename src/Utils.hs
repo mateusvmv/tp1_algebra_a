b4f2c5b84fffc0adfa9f24d0e399bfe57301fefc
luishgh <51053603+luishgh@users.noreply.github.com>
2024-05-16 19:50:39 +0000

    [WIP] Something that apparently works, yey

--- src/Utils.hs
+++ src/Utils.hs
@@ -14,3 +14,22 @@ binExp a b p = f' 1 a b where
       | b == 0 = r
       | odd b = f' (r*a `mod` p) (a^2 `mod` p) (shiftR b 1)
       | otherwise = f' r (a^2 `mod` p) (shiftR b 1)
+
+-- Calcula o gcd extendido de a e b, retorna (gcd(a, b), x, y) tais
+-- que ax + by = gcd(a, b)
+egcd :: Integer -> Integer -> (Integer, Integer, Integer)
+egcd a 0 = (a, 1, 0)
+egcd a b =
+    let (g, x1, y1) = egcd b r
+    in (g, y1, x1 - y1 * q)
+       where
+         (q, r) = divMod a b
+
+-- Calcula o inverso modular de a mod m
+invMod :: Integer -> Integer -> Integer
+invMod a m =
+    let (g, x, y) = egcd a m
+    in case g of
+      1 -> (x `mod` m + m) `mod` m
+      _ -> error $ "invMod expects coprime numbers, received " ++ show a ++ " " ++ show m
+         
