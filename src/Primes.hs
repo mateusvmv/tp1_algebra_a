0f1802e39e27a1ce3997572085f2f231eb53d35f
mateusvmv <mateusv251@gmail.com>
2024-05-17 16:57:38 -0300

    fatoração com pollard rho

--- src/Primes.hs
+++ src/Primes.hs
@@ -28,9 +28,10 @@ wheel5Cycle :: Integer = product $ take 5 primes
 -- Miller Rabin
 isPrime :: Integer -> Bool
 isPrime n
-  | n < 10^12 = length (factorize n) == 1
-  | any (\p -> n `mod` p == 0) (take 1000 primes) = False
+  | n < 10^12 = isCoprime n (takeWhile (\p -> p*p <= n) primes)
+  | not $ isCoprime n (take 1000 primes) = False
   | otherwise = (not . any witness) (take 50 primes) where
+    isCoprime n ps = not $ any (\p -> mod n p == 0) ps
     (s, d) = f' (0, n-1) where
         f' (s, n)
           | even n = f' (s+1, shiftR n 1)
@@ -51,31 +52,51 @@ firstPrimeGT n
         candidates = dropWhile (<= n) wheel
     in head [x | x <- candidates, isPrime x]
 
+-- Retorna um fator com pollardRho
+pollardRho :: Integer -> Integer -> Integer
+pollardRho n lim = step 1 2 n 2 2 where
+    step i k n x y
+        | d /= 1 && d /= n = d
+        | i >= lim = 1
+        | i == k = step (i+1) (2*k) n x' x'
+        | otherwise = step (i+1) k n x' y where
+            d = gcd n (abs y-x)
+            x' = (\x n -> mod (x*x-1) n) x n
+
 -- Retorna a lista de fatores de um numero
 factorize :: Integer -> [Integer]
-factorize 1 = []
-factorize n = factorize' n primes where
-    factorize' :: Integer -> [Integer] -> [Integer]
-    factorize' n (p:ps)
-        | p*p > n = [n]
-        | n `rem` p == 0 = p : factorize' (n `div` p) (p:ps)
-        | otherwise =  factorize' n ps
+factorize' n ps
+    | r == 1 = fs
+    | otherwise = sort $ fs ++ factorize' f ps' ++ factorize' (div r f) ps'
+    where
+        f = pollardRho r r
+        (fs, ps', r) = trialDiv n ps
+factorize n = sort $ factorize' n primes
 
 -- Fatoração parcial
-data Factorization = Full [Integer] | Partial [Integer]
+data Factorization = Full [Integer] | Partial [Integer] Integer
 
 instance Show Factorization where
     show (Full fs) = "Full factorization: " ++ show fs
-    show (Partial fs) = "Partial factorization: " ++ show fs
+    show (Partial fs r) = "Partial factorization: " ++ show fs ++ " * " ++ show r
+
+trialDiv :: Integer -> [Integer] -> ([Integer], [Integer], Integer)
+trialDiv n (p:ps)
+    | n == 1 = ([], [], 1)
+    | p*p > n = ([n], [], 1)
+    | mod n p == 0 = (p:fs', ps', r)
+    | p < 10^6 = trialDiv n ps
+    | isPrime n = ([n], [], 1)
+    | otherwise = ([], ps, n)
+    where (fs', ps', r) = trialDiv (div n p) (p:ps)
 
 factorizeHuge :: Integer -> Factorization
-factorizeHuge n = factorize' n primes
-  where
-    factorize' :: Integer -> [Integer] -> Factorization
-    factorize' n (p:ps)
-      | p > 10^6 = Partial []
-      | p * p > n = Full [n]
-      | n `rem` p == 0 = case factorize' (n `div` p) (p:ps) of
-        Full fs -> Full (p:fs)
-        Partial fs -> Partial (p:fs)
-      | otherwise = factorize' n ps
+factorizeHuge' n ps
+    | r == 1 || f == 1 = fs
+    | otherwise = sort $ (fs ++ factorizeHuge' f ps') ++ factorizeHuge' (div r f) ps'
+    where
+        f = pollardRho r (10^6)
+        (fs, ps', r) = trialDiv n ps
+factorizeHuge n = if p == n then Full fs else Partial fs (div n p) where
+    p = product fs
+    fs = factorizeHuge' n primes
